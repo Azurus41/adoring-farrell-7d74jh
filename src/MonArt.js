329244479edbf7b17174334ec5ec25385871b93f
azurus41 <140336941+Azurus41@users.noreply.github.com>
2026-03-25 08:45:22 +0000

    v1

---
+++ src/MonArt.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function MonArt() {
+    return (
+      <div className="MonArt">
+        <h2>Notre métier : vous accompagner !</h2>
+        
+        <ul>
+            <li>Référencement</li>
+            <li>Rédaction Web</li>
+            <li>Google Analytics</li>
+        </ul>
+      </div>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import Moncomp from "./Moncomp";
+import MonArt from "./MonArt";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +11,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Moncomp />
   </StrictMode>
 );
